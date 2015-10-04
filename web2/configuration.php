173d21b60002490bdce552daa06349be0153a375
saminaa <saminaa@direct.fifalike.net>
2015-10-04 23:23:18 +0330

    web2

--- web2/configuration.php
+++ web2/configuration.php
@@ -54,8 +54,8 @@ class JConfig {
 	public $sef_suffix = '0';
 	public $unicodeslugs = '0';
 	public $feed_limit = '10';
-	public $log_path = '/home2/saminaa/public_html/logs';
-	public $tmp_path = '/home2/saminaa/public_html/tmp';
+	public $log_path = '/home2/saminaa/public_html/sana/web2/logs';
+	public $tmp_path = '/home2/saminaa/public_html/sana/web2/tmp';
 	public $lifetime = '15';
 	public $session_handler = 'database';
 }
